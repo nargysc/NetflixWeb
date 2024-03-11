4c5211d3d1f355481a4a8807fb2c6e70c9211694
nargysc <eliasnargiz@gmail.com>
2024-03-11 18:22:36 +0200

    Deleted unnecessary code

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDn80PLDWxwTJbn7ix2I-BAddc02qeDtJ8",
+  authDomain: "netflixweb-3dcfd.firebaseapp.com",
+  projectId: "netflixweb-3dcfd",
+  storageBucket: "netflixweb-3dcfd.appspot.com",
+  messagingSenderId: "466763268824",
+  appId: "1:466763268824:web:be5f52bff402a2d0db4dad",
+  measurementId: "G-FQQGBX725M"
+};
