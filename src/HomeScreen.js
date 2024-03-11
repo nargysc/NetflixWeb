0e794ec6e9b3fbba8a2acce4d0f10b6756c50efd
nargysc <eliasnargiz@gmail.com>
2024-03-11 18:54:06 +0200

    Added Nav to HomeScreen

---
+++ src/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+
+function HomeScreen() {
+  return <div class="homeScreen">
+    {
+        /*nav*/
+        <Nav/>
+    }
+    {
+        /*banner*/
+    }
+    {
+        /*row*/
+    }
+  </div>
+}
+
+export default HomeScreen;
