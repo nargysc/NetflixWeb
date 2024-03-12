0ff3669224bb5a2cf4ef7c214d6fbb121bf10087
nargysc <eliasnargiz@gmail.com>
2024-03-12 17:52:10 +0200

    API added

--- src/Banner.js
+++ src/Banner.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Banner.css";
+import requests from "./Requests.js";
+import axios from "./axios";
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
+    useEffect (() =>
+{
+    async function fetchData() {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        setMovie (
+            request.data.results[
+                Math.floor(Math.random() * request.data.results.length -1)
+            ]
+        );
+        return request;
+    }
+    fetchData();
+}, [])
+    function truncate(string, n) {
+        return string?.length > n ? string.subtr(0, n - 1) + '...' : string;
+    }
   return <header 
   className='banner' 
   style={
     {
-        backgroundImage:`url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC')`,
+        backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundSize: "cover",
         backgroundPosition: "center center",
     }
@@ -17,7 +36,17 @@ function Banner() {
         <button className='banner__button'>Play</button>
         <button className='banner__button'>My List</button>
     </div>
-    <h1 className='banner__description'>test test test</h1>
+    <h1 className='banner__description'>{
+        truncate(
+        `test test test
+        test test test
+        test test test
+        test test test
+        test test test
+        test test test`, 150
+        )
+    }
+    </h1>
     </div> 
     <div className="banner--fadeBottom">
     </div>
