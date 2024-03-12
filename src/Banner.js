a3e29fe781d3ee12361676e9b402ff26a7f8b42c
nargysc <eliasnargiz@gmail.com>
2024-03-12 17:03:48 +0200

    Fixed banner fade bottom , added buttons css

--- src/Banner.js
+++ src/Banner.js
@@ -2,11 +2,27 @@ import React from 'react'
 import "./Banner.css";
 
 function Banner() {
-  return (
-    <div>
-      
+  return <header 
+  className='banner' 
+  style={
+    {
+        backgroundImage:`url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+    }
+  }>
+<div className='banner__contents'>
+    <h1 className='banner__title'>Movie Name</h1>
+    <div className='banner__buttons'>
+        <button className='banner__button'>Play</button>
+        <button className='banner__button'>My List</button>
     </div>
-  )
+    <h1 className='banner__description'>test test test</h1>
+    </div> 
+    <div className="banner--fadeBottom">
+    </div>
+
+  </header>
 }
 
 export default Banner
