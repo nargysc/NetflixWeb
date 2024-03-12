82c59177033d419ae5312f0645646d17b818b61a
nargysc <eliasnargiz@gmail.com>
2024-03-12 15:44:51 +0200

    Fixed Nav

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import HomeScreen from "./HomeScreen";
 function App() {
   return (
     <div className="App">
-      Hello
       <HomeScreen/>
     </div>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -2,9 +2,20 @@ import React from 'react'
 import "./Nav.css";
 
 function Nav() {
-  return <div class="nav">
+  return ( <div class="nav">
+    <div class="nav nav__black">
+  <div class="nav__contents">
+      <img 
+    className="nav__logo"
+    src="https://pngimg.com/d/netflix_PNG10.png" alt="" />
 
+    <img
+    className="nav__avatar"
+     src="https://i.pinimg.com/474x/44/78/1b/44781ba4ac0c63f2ecf0793a533bf8dc.jpg" alt="" />
   </div>
-}
+</div>
+  </div>
+  )
+};
 
 export default Nav
