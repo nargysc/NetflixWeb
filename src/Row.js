19397c7411817edf5e7463290911cfc114740e26
nargysc <eliasnargiz@gmail.com>
2024-03-13 01:12:50 +0200

    Fixed row component, added firebase

--- src/Row.js
+++ src/Row.js
@@ -17,17 +17,25 @@ async function fetchData() {
   return (
     <div className='row'>
       <h2>{title}</h2>
+
       <div className='row__posters'>
-      {movies.map(movie => (
-        <img  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+      {movies.map(
+        (movie) => 
+    ((isLargeRow && movie.poster_path) ||
+      (!isLargeRow && movie.backdrop_path)) && (
+         <img  
+        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
         key={movie.id}
         src={`${base_url}${
             isLargeRow ? movie.poster_path : movie.backdrop_path
-        }`} alt={movie.name} />
-      ))}
+        }`} 
+        alt={movie.name} 
+        />
+      
+    ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,13 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDn80PLDWxwTJbn7ix2I-BAddc02qeDtJ8",
   authDomain: "netflixweb-3dcfd.firebaseapp.com",
   projectId: "netflixweb-3dcfd",
   storageBucket: "netflixweb-3dcfd.appspot.com",
   messagingSenderId: "466763268824",
-  appId: "1:466763268824:web:be5f52bff402a2d0db4dad",
-  measurementId: "G-FQQGBX725M"
+  appId: "1:466763268824:web:70f9781309bcc01cdb4dad",
+  measurementId: "G-C288MDNXMZ"
 };
+
+const app = initializeApp(firebaseConfig);
