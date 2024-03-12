1db4cd24100f37fd1af5340c239ace6e3411b666
nargysc <eliasnargiz@gmail.com>
2024-03-12 18:30:04 +0200

    Checking the app

--- src/Banner.js
+++ src/Banner.js
@@ -32,22 +32,13 @@ function Banner() {
   }>
 <div className='banner__contents'>
     <h1 className='banner__title'>{
-        movie?.title || movie?.name || movie?.original_name
-}</h1>
+        movie?.title || movie?.name || movie?.original_name}</h1>
     <div className='banner__buttons'>
         <button className='banner__button'>Play</button>
         <button className='banner__button'>My List</button>
     </div>
     <h1 className='banner__description'>{
-        truncate(
-        `test test test
-        test test test
-        test test test
-        test test test
-        test test test
-        test test test`, 150
-        )
-    }
+        truncate(movie?.overview, 150)}
     </h1>
     </div> 
     <div className="banner--fadeBottom">
