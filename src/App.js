173016a59fa8ebb7d0865eac0595035523a67ecb
nargysc <eliasnargiz@gmail.com>
2024-03-14 22:39:31 +0200

    Login Screen Added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter as Router, Switch,  Route} from "react-router-dom";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
       <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
         <Switch>
           <Route exact path="/">
             <HomeScreen/>
             </Route>
         </Switch>
+  )}
       </Router>
     </div>
   );
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./LoginScreen.css";
+
+function LoginScreen() {
+  return  (
+  <div className='loginScreen'>
+        <img className='loginScreen__logo'
+        src="https://pngimg.com/d/netflix_PNG10.png"
+         alt="" />
+         <button className='loginScreen__button'>
+            Sign In
+         </button>
+    </div>
+  );
+}
+
+export default LoginScreen;
