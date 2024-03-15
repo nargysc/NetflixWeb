5572f394a265ebd34917f2f9d9df821cf7148ea2
nargysc <eliasnargiz@gmail.com>
2024-03-15 16:29:06 +0200

    Added Login Screen Body

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./LoginScreen.css";
 
 function LoginScreen() {
-  return  (
-  <div className='loginScreen'>
-        <img className='loginScreen__logo'
+  const [signIn, setSignIn] = useState(false);
+  return (
+    <div className="loginScreen">
+      <div className='loginScreen__background'>
+      <img
+        className="loginScreen__logo"
         src="https://pngimg.com/d/netflix_PNG10.png"
-         alt="" />
-         <button className='loginScreen__button'>
-            Sign In
-         </button>
-         <div className = "loginScreen__gradient" >
+        alt=""
+      />
+      <button 
+      onClick={() => setSignIn(true)}
+      className="loginScreen__button">Sign In</button>
+      <div className="loginScreen__gradient"/>
+      </div>
 
-         </div>
-    </div>
+      <div className="loginScreen__body">
+        <>
+          <h1>Unlimited Movies, TV Shows and more.</h1>
+          <h2>Watch anywehere, cancel anytime</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+          <div class="loginScreen__input">
+            <form>
+              <input type="email"
+              placeholder='Email Adress' />
+              <button onClick={() => setSignIn(true)}
+              className='loginScreen__getStarted'>
+                Get Started
+              </button>
+            </form>
+          </div>
+        </>
+      </div>
+      </div>
   ); 
 }
 
